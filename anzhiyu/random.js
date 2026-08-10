65b355f3498218d412ec255bc1e844f8e35257d3
Crypto1Long <2390999795@qq.com>
2026-08-10 20:38:04 +0800

    Site updated: 2026-08-10 20:38:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/07/api学习/"];function toRandomPost(){
+var posts=["2026/08/07/api学习/api学习/","2026/08/10/注册表学习/注册表的学习/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
